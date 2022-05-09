43380dec077165b67a0ea2cb3571656cbfcf56d9
PankovGV <dooz2006@mail.ru>
2022-05-09 18:30:19 +0300

    Change ColorView

--- SelectionOfFlowers/ViewController.swift
+++ SelectionOfFlowers/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var meaningRedSlider: UILabel!
+    @IBOutlet var meaningGreenSlider: UILabel!
+    @IBOutlet var meaningBlueSlider: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorChangeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorChangeView.layer.cornerRadius = 10
+        
+        redSlider.value = 0.10
+        greenSlider.value = 0.25
+        blueSlider.value = 0.50
+        
+        meaningRedSlider.text = String(redSlider.value)
+        meaningGreenSlider.text = String(greenSlider.value)
+        meaningBlueSlider.text = String(blueSlider.value)
+        
+        redLabel.textColor = .red
+        greenLabel.textColor = .green
+        blueLabel.textColor = .blue
+        
     }
-
-
+    
+    @IBAction func changeTheAmountOfRed() {
+        meaningRedSlider.text = String(format: "%.2f", redSlider.value)
+        meaningGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        meaningBlueSlider.text = String(format: "%.2f", blueSlider.value)
+        
+        changeColorView()
+    }
+    
+    func changeColorView() {
+            colorChangeView.backgroundColor = UIColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1
+            )
+        }
+    
 }
 
